7ac4841f245eb53bba96f64d540cc5c906e4c46d
Samuel Romero Arbelo <oiranca@gmail.com>
2019-09-29 19:31:27 +0100

    New View Login adn Sign Up

--- app/src/main/java/com/oiranca/pglproject/MainActivity.java
+++ app/src/main/java/com/oiranca/pglproject/MainActivity.java
@@ -2,13 +2,33 @@ package com.oiranca.pglproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    TextView sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    sign = (TextView) findViewById(R.id.text_sign);
+    sign.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent signIntent = new Intent(getApplicationContext(),ActivitySignUp.class);
+            startActivity(signIntent);
+        }
+    });
+
     }
+
+
+
 }
