2f67770006c67413654558d2d806cc04111f2ca1
Samuel Romero Arbelo <oiranca@gmail.com>
2019-10-02 20:08:17 +0100

    New Color Spinner

--- app/src/main/java/com/oiranca/pglproject/MainActivity.java
+++ app/src/main/java/com/oiranca/pglproject/MainActivity.java
@@ -6,9 +6,9 @@ import androidx.fragment.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+
 import android.widget.Button;
-import android.widget.Spinner;
+
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
