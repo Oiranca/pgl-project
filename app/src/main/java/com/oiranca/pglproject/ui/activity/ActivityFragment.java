e392e3fb45d696906e4422a89eb5d739e57e46b3
Samuel Romero Arbelo <oiranca@gmail.com>
2019-10-21 17:19:34 +0100

    New Button Login

--- app/src/main/java/com/oiranca/pglproject/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/oiranca/pglproject/ui/activity/ActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -24,6 +25,9 @@ public class ActivityFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         activityViewModel =ViewModelProviders.of(this).get(ActivityViewModel.class);
         View root = inflater.inflate(R.layout.fragment_activity, container, false);
+        Spinner spinnerFam = root.findViewById(R.id.spinnerFamily);
+
+
         final TextView textView = root.findViewById(R.id.home);
         activityViewModel.getText().observe(this, new Observer<String>() {
             @Override
