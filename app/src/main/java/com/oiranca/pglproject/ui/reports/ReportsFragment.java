9ac7fe7d4d95dc647fbe460ef46b627580d0f3cd
Samuel Romero Arbelo <oiranca@gmail.com>
2019-10-22 22:05:10 +0100

    Insert data in TableLayout

--- app/src/main/java/com/oiranca/pglproject/ui/reports/ReportsFragment.java
+++ app/src/main/java/com/oiranca/pglproject/ui/reports/ReportsFragment.java
@@ -1,45 +1,81 @@
 package com.oiranca.pglproject.ui.reports;
 
+import android.app.DatePickerDialog;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.media.RatingCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.oiranca.pglproject.R;
 
+import java.util.ArrayList;
+
 public class ReportsFragment extends Fragment {
 
     private ReportsViewModel reportsViewModel;
+    TableLayout tableLayout;
+    String[] header = {"Fecha", "Nombre", "Actividad"};
+    ArrayList<String[]> rows = new ArrayList<>();
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         reportsViewModel =
                 ViewModelProviders.of(this).get(ReportsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_reports, container, false);
+        tableLayout = (TableLayout) root.findViewById(R.id.tableReports);
 
+        TableView tablaDyn = new TableView(tableLayout, getContext());
+        tablaDyn.setHeader(header);
+        tablaDyn.setData(getClients());
+        tablaDyn.backgroundHeader(Color.parseColor("#0081D4"));
+        tablaDyn.backgroundData(Color.parseColor("#B2D0F0"));
 
 
 // Aquí pondríamos los datos de las actividades en la base de datos al pulsar el calendario
 
 
         CalendarView calenReport = root.findViewById(R.id.calendarReport);
+
         calenReport.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
 
 
-                Toast.makeText(getContext(),"Cargar la tabla con: "+dayOfMonth+"/"+month+"/"+year,Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(getContext(), "Cargar la tabla con: " + dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
 
 
             }
         });
 
+
         return root;
     }
+
+
+
+    private ArrayList<String[]> getClients() {
+
+        rows.add(new String[]{"01.01.2000", "Samuel", "Limpiar"});
+        rows.add(new String[]{"02.01.2000", "Pedro", "Lavar"});
+        rows.add(new String[]{"03.01.2000", "Juan", "Perro"});
+        rows.add(new String[]{"04.01.2000", "Jose", "Secar"});
+        return rows;
+    }
+
 }
---
+++ app/src/main/java/com/oiranca/pglproject/ui/reports/TableView.java
@@ -0,0 +1,164 @@
+package com.oiranca.pglproject.ui.reports;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class TableView {
+
+    private TableLayout tableLayout;
+    private Context context;
+    private String[] header;
+    private ArrayList<String[]> data;
+    private TableRow tableRow;
+    private TextView txtCell;
+    private int indexC, indexR;
+
+    private int firstColor;
+
+
+    public TableView(TableLayout tableLayout, Context context) {
+
+        this.tableLayout = tableLayout;
+        this.context = context;
+
+    }
+
+
+    public void setHeader(String[] header) {
+        this.header = header;
+        createHeader();
+    }
+
+
+    public void setData(ArrayList<String[]> data) {
+        this.data = data;
+        createDataTable();
+    }
+
+    private void newRow() {
+
+        tableRow = new TableRow(context);
+
+    }
+
+    private void newCell() {
+
+        txtCell = new TextView(context);
+        txtCell.setGravity(Gravity.CENTER);
+        txtCell.setTextSize(16);
+
+
+    }
+
+    private void createHeader() {
+        indexC = 0;
+        newRow();
+        while (indexC < header.length) {
+            newCell();
+            txtCell.setText(header[indexC++]);
+            tableRow.addView(txtCell, newTableRowParams());
+
+        }
+        tableLayout.addView(tableRow);
+    }
+
+    private void createDataTable() {
+        String info;
+
+        for (indexR = 1; indexR <= header.length; indexR++) {
+            newRow();
+
+            for (indexC = 0; indexC < header.length; indexC++) {
+                newCell();
+                String[] columns = data.get(indexR - 1);
+                info = (indexC < columns.length) ? columns[indexC] : "";
+                txtCell.setText(info);
+                tableRow.addView(txtCell, newTableRowParams());
+
+
+            }
+            tableLayout.addView(tableRow);
+
+        }
+
+
+    }
+
+    public void backgroundHeader(int color) {
+
+        indexC = 0;
+        newRow();
+        while (indexC < header.length) {
+            txtCell = getCell(0, indexC++);
+            txtCell.setBackgroundColor(color);
+            txtCell.setTypeface(null, Typeface.BOLD);
+
+        }
+
+
+    }
+
+
+    public void backgroundData(int firstColor) {
+
+        for (indexR = 1; indexR <= header.length; indexR++) {
+
+
+            for (indexC = 0; indexC < header.length; indexC++) {
+                txtCell = getCell(indexR, indexC);
+                txtCell.setBackgroundColor(firstColor);
+
+
+            }
+
+
+        }
+
+        this.firstColor = firstColor;
+
+
+    }
+
+    public void reColoring() {
+
+        indexC = 0;
+
+        while (indexC < header.length) {
+            txtCell = getCell(data.size() - 1, indexC++);
+            txtCell.setBackgroundColor(firstColor);
+
+        }
+
+
+    }
+
+    private TableRow getRow(int index) {
+        return (TableRow) tableLayout.getChildAt(index);
+
+
+    }
+
+    private TextView getCell(int rowIndex, int columsIndex) {
+
+        tableRow = getRow(rowIndex);
+        return (TextView) tableRow.getChildAt(columsIndex);
+
+
+    }
+
+    private TableRow.LayoutParams newTableRowParams() {
+
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.setMargins(1, 1, 1, 1);
+        params.weight = 2;
+        return params;
+
+    }
+}
+
