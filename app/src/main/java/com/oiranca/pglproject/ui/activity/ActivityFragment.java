c33216b4eb1f6cebe24581315b90d3db9f9835f6
Samuel Romero Arbelo <oiranca@gmail.com>
2020-01-09 13:22:45 +0000

    Insert data in Spinner Family

--- app/src/main/java/com/oiranca/pglproject/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/oiranca/pglproject/ui/activity/ActivityFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -14,19 +17,127 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.oiranca.pglproject.R;
+
+import java.util.ArrayList;
 
 public class ActivityFragment extends Fragment {
 
     private ActivityViewModel activityViewModel;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ArrayAdapter<String> comAdp;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        activityViewModel =ViewModelProviders.of(this).get(ActivityViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_activity, container, false);
-        Spinner spinnerFam = root.findViewById(R.id.spinnerFamily);
+        activityViewModel = ViewModelProviders.of(this).get(ActivityViewModel.class);
+        final View root = inflater.inflate(R.layout.fragment_activity, container, false);
+        final Spinner spinnerFam = root.findViewById(R.id.spinnerFamily);
+
+        final Spinner spinnerWork = root.findViewById(R.id.spinnerWork);
+
+
+        Intent idUser = getActivity().getIntent();
+        Bundle user = idUser.getExtras();
+        String emailUser;
+
+        emailUser = user.getString("Admin");
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("Family-" + emailUser.replace(".", "-"));
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<String> nameFamily = new ArrayList<>();
+
+                for (DataSnapshot name : dataSnapshot.getChildren()) {
+                    String namesAd = name.child("name").getValue(String.class);
+                    String nameFm = name.child("nameF").getValue(String.class);
+                    if (nameFm != null) {
+                        nameFamily.add(nameFm);
+                    }
+                    if ((namesAd != null)) {
+                        nameFamily.add(namesAd);
+                    }
+                }
+
+                comAdp = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, nameFamily);
+                spinnerFam.setAdapter(comAdp);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        spinnerFam.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                final String famSelect = spinnerFam.getSelectedItem().toString();
+                spinnerWork.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        final String workSelect = spinnerWork.getSelectedItem().toString();
+                        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                String email;
+                                for (DataSnapshot work : dataSnapshot.getChildren()) {
+                                    String namesAd = work.child("name").getValue(String.class);
+                                    String namesFam = work.child("nameF").getValue(String.class);
+
+                                    if (namesAd != null) {
+                                        if (namesAd.contains(famSelect)) {
+                                            email = work.child("email").getValue(String.class);
+                                            Toast.makeText(getContext(), email, Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+
+                                    if (namesFam != null) {
+                                        if (namesFam.contains(famSelect)) {
+                                            email = work.child("emailF").getValue(String.class);
+                                            Toast.makeText(getContext(), email, Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+
+
+
+                                }
+                            }
+
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         final TextView textView = root.findViewById(R.id.home);
@@ -34,9 +145,11 @@ public class ActivityFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
 
-                    textView.setText(s);
+                textView.setText(s);
             }
         });
+
+
         return root;
     }
 }
